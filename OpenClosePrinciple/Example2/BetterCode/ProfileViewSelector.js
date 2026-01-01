1ee5d896407965b19492d32ba6952db95f98cc04
arjunpjaiswal <arjunphj@gmail.com>
2026-01-02 00:45:13 +0530

    added OCP example 2 JS based

---
+++ OpenClosePrinciple/Example2/BetterCode/ProfileViewSelector.js
@@ -0,0 +1,10 @@
+const ProfileViewSelector(role){
+getProfileView(role){
+switch(role){
+case 'admin':return<AdminView/>
+case 'editor':return<EditorView>
+default :return <StardView/>
+}
+}
+}
+export default ProfileViewSelector;
